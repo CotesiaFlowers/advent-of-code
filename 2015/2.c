443dda01893be0483fc057046dcb18241a6c27af
CotesiaFlowers <cotesiaflowers@proton.me>
2025-02-27 14:27:59 +0000

    style

--- 2015/2.c
+++ 2015/2.c
@@ -6,45 +6,49 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-int readnum(char **s) {
-	char c;
+// int readnum(char **s) {
+// 	char c;
 
-	int n=0, r=0; // nth digit, result
+// 	int n=0, r=0; // nth digit, result
 
-	do { c = (*s)[++n]; } while (isdigit(c));
+// 	do { c = (*s)[++n]; } while (isdigit(c));
 	
-	c = **s;
-	for (;n;n--) {
-		r += ((int)c-'0') * pow(10, n-1);
-		c = *++(*s);
-	}
+// 	c = **s;
+// 	for (;n;n--) {
+// 		r += ((int)c-'0') * pow(10, n-1);
+// 		c = *++(*s);
+// 	}
 	
-	while (!isdigit(c) && c!=EOF) c = *++(*s);
+// 	while (!isdigit(c) && c!=EOF) c = *++(*s);
 
-	return r;
-}
+// 	return r;
+// }
 
 int main() {
 	FILE *f = fopen("input2", "r");
 	fseek(f, 0, SEEK_END);
 	long size = ftell(f);
 	rewind(f);
-	char *input = (char*) malloc(size);
-	fread(input, size, 1, f);
+	char *s = (char*) malloc(size);
+	fread(s, size, 1, f);
 	int paper=0, ribbon=0;
-	input[size]=EOF;
+	s[size]=EOF;
 
 	do {
 		int l=0 ,w=0, h=0;
-		l=readnum(&input);
-		w=readnum(&input);
-		h=readnum(&input);
+		int o=0; // offset
+		// l=readnum(&input);
+		// w=readnum(&input);
+		// h=readnum(&input);
+		sscanf(s, "%d%n", &l, &o); s+=o+1;
+		sscanf(s, "%d%n", &w, &o); s+=o+1;
+		sscanf(s, "%d%n", &h, &o); s+=o+1;
 		int a = MIN(MIN(l,w), h); // smallest axis
 		int b = MIN(MAX(l,h), MIN(MAX(l,w), MAX(w,h))); // second smallest axis
 		paper += a * b + 2*l*w + 2*w*h + 2*h*l;
 		ribbon += 2*a + 2*b + l*w*h;
 
-	} while (*input != EOF);
+	} while (*s != EOF);
 
 	printf("wrapping paper: %dft\n", paper);
 	printf("ribbon: %dft\n", ribbon);
